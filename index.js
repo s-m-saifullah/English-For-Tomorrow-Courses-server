ffa7a14286f75b57d6f432deee1a85efae035acd
shaquille19 <shakil.m.19@gmail.com>
2022-10-24 22:53:15 +0600

    added courses.json and created api

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+
 const cors = require("cors");
 app.use(cors());
+
+const courses = require("./data/courses.json");
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const selectedCourse = courses.find((course) => course.id === id);
+  res.send(selectedCourse);
+});
+
+app.listen(port, console.log("Server Running on Port", port));
